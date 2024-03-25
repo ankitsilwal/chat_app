7625f999463dd23af6ce16c12de55c53203483ea
ankitsilwal <ankitk5620@gmail.com>
2024-03-25 22:02:14 +0530

    update:change credentials

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXxsA8h-eI_mgUFpl51LAWrSR-n_k5gWM",
-  authDomain: "new-chat-be1ca.firebaseapp.com",
-  projectId: "new-chat-be1ca",
-  storageBucket: "new-chat-be1ca.appspot.com",
-  messagingSenderId: "596072760775",
-  appId: "1:596072760775:web:2107b95fa7b1e8e5f645e2",
-  measurementId: "G-CGBW6D0JG0"
+  apiKey: "AIzaSyDExpDA0tL9CwfRwFs7K0eZzw7LziHCXus",
+  authDomain: "chatapp-7929a.firebaseapp.com",
+  projectId: "chatapp-7929a",
+  storageBucket: "chatapp-7929a.appspot.com",
+  messagingSenderId: "203506636086",
+  appId: "1:203506636086:web:67ab994439e82cd4080f97",
+  measurementId: "G-SGY53FQESE"
 };
 
 // Initialize Firebase
